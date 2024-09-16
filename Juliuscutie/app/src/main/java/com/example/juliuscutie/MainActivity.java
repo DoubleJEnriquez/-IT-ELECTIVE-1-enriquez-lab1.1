1950106fbf01357aaad3bffa203f0e7a46b4dea8
DoubleJEnriquez <juliusjohnvenriquez@gmail.com>
2024-09-17 00:51:35 +0800

    Add files via upload

---
+++ Juliuscutie/app/src/main/java/com/example/juliuscutie/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.juliuscutie;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nameInput, emailInput, phoneInput, addressInput, aboutMeInput;
+    private Button saveButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        phoneInput = findViewById(R.id.phoneInput);
+        emailInput = findViewById(R.id.emailInput);
+        nameInput = findViewById(R.id.nameInput);
+        addressInput = findViewById(R.id.addressInput);
+        aboutMeInput = findViewById(R.id.aboutMeInput);
+        saveButton = findViewById(R.id.saveButton);
+
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name = nameInput.getText().toString();
+                String email = emailInput.getText().toString();
+                String phone = phoneInput.getText().toString();
+                String address = addressInput.getText().toString();
+                String aboutMe = aboutMeInput.getText().toString();
+
+
+                String message = "Name: " + name + "\nEmail: " + email + "\nPhone: " + phone + "\nAddress: " + address
+                        + "\nAbout Me: " + aboutMe;
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
